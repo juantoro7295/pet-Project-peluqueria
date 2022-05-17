dad657960251f3496696758f4a693e997b38dcf2
Juanes07 <juanesdm3@gmail.com>
2022-05-17 16:33:38 -0500

    Caracteristicas Producto
    Se agregan las caracteristicas correspondientes de la entidad Producto

--- Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/Producto.java
+++ Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/Producto.java
@@ -1,8 +1,20 @@
 package co.com.salondebelleza.belleza.puestodetrabajo;
 
+import co.com.salondebelleza.belleza.puestodetrabajo.values.Categoria;
 import co.com.sofka.domain.generic.Entity;
+import generic.values.Nombre;
 
 public class Producto extends Entity<ProductoId> {
+
+    protected Nombre nombre;
+    protected Categoria categoria;
+
+    public Producto(ProductoId id, Nombre nombre, Categoria categoria) {
+        super(id);
+        this.nombre = nombre;
+        this.categoria = categoria;
+    }
+
     public Producto(ProductoId id) {
         super(id);
     }
