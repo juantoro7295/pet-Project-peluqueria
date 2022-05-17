003196628a42c1d20e052b04ea6ce375453d0c57
Juanes07 <juanesdm3@gmail.com>
2022-05-17 16:28:57 -0500

    Entidad Producto
    Se crea la entidad Producto

---
+++ Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/Producto.java
@@ -0,0 +1,9 @@
+package co.com.salondebelleza.belleza.puestodetrabajo;
+
+import co.com.sofka.domain.generic.Entity;
+
+public class Producto extends Entity<ProductoId> {
+    public Producto(ProductoId id) {
+        super(id);
+    }
+}
