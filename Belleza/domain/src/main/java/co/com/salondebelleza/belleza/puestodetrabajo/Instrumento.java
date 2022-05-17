175afae61fe229fcd31097d5e55f7fbad24bc739
Juanes07 <juanesdm3@gmail.com>
2022-05-17 16:31:53 -0500

    Caracteristicas Instrumento
    Se agregan las caracteristicas de la entidad Instrumento

--- Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/Instrumento.java
+++ Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/Instrumento.java
@@ -1,8 +1,20 @@
 package co.com.salondebelleza.belleza.puestodetrabajo;
 
+import co.com.salondebelleza.belleza.puestodetrabajo.values.Categoria;
 import co.com.sofka.domain.generic.Entity;
+import generic.values.Nombre;
 
 public class Instrumento extends Entity<InstrumentoId> {
+
+    protected Nombre nombre;
+    protected Categoria categoria;
+
+    public Instrumento(InstrumentoId id, Nombre nombre, Categoria categoria) {
+        super(id);
+        this.nombre = nombre;
+        this.categoria = categoria;
+    }
+
     public Instrumento(InstrumentoId id) {
         super(id);
     }
