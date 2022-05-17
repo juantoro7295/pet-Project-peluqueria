35605370b75a56886a7999634e9a5587343665f9
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-17 15:57:21 -0500

    Correccion Objeto de Valor - TipoContrato
    Se corrige una caracterisitica de el objeto de valor

--- Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/values/TipoContrato.java
+++ Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/values/TipoContrato.java
@@ -6,11 +6,16 @@ import java.util.Objects;
 
 public class TipoContrato implements ValueObject<String> {
     private final String tipoDeContrato;
+    private final String descripcion;
 
-    public TipoContrato(String tipoDeContrato) {
+    public TipoContrato(String tipoDeContrato, String descripcion) {
         this.tipoDeContrato = Objects.requireNonNull(tipoDeContrato);
         if (this.tipoDeContrato.isBlank()) {
             throw new IllegalArgumentException("El tipo de contrato no puede estar vacio");
+        }
+        this.descripcion = Objects.requireNonNull(descripcion);
+        if (this.descripcion.isBlank()) {
+            throw new IllegalArgumentException("La descripcion no puede estar vacia");
         }
     }
 
