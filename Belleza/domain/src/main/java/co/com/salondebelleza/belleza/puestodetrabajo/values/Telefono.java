ff932b7100cabc9cea9915b8d42b9c89c8c8eb9b
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-17 15:49:14 -0500

    Objeto de valor - Telefono
    Se agrega el objeto de valor telefono

---
+++ Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/values/Telefono.java
@@ -0,0 +1,21 @@
+package co.com.salondebelleza.belleza.puestodetrabajo.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Telefono implements ValueObject<String> {
+    private final String telefono;
+
+    public Telefono(String telefono) {
+        this.telefono = Objects.requireNonNull(telefono);
+        if (this.telefono.isBlank()) {
+            throw new IllegalArgumentException("El telefono no puede estar vacio");
+        }
+    }
+
+    @Override
+    public String value() {
+        return telefono;
+    }
+}
