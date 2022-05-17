4ee06e2a9bb4386d9ecbaf82724ce24da827e086
Camila Morales <camila.moralesg21@gmail.com>
2022-05-17 17:22:00 -0500

    Crear Contrato
    Se crea la entidad Contrato y se agregan sus comportamientos

---
+++ Belleza/domain/src/main/java/co/com/salondebelleza/belleza/empleado/Contrato.java
@@ -0,0 +1,23 @@
+package co.com.salondebelleza.belleza.empleado;
+
+import co.com.salondebelleza.belleza.empleado.values.ContratoId;
+import co.com.salondebelleza.belleza.empleado.values.TipoContrato;
+import co.com.sofka.domain.generic.Entity;
+
+public class Contrato extends Entity<ContratoId> {
+
+    protected TipoContrato tipoContrato;
+
+    public Contrato(ContratoId entityId) {
+        super(entityId);
+    }
+
+    public Contrato(ContratoId entityId, TipoContrato tipoContrato) {
+        super(entityId);
+        this.tipoContrato = tipoContrato;
+    }
+
+    public void crearTipoContrato(String tipoContrato, String descripcion){
+        this.tipoContrato = new TipoContrato(tipoContrato, descripcion);
+    }
+}
---
+++ Belleza/domain/src/main/java/co/com/salondebelleza/belleza/empleado/values/ContratoId.java
@@ -0,0 +1,9 @@
+package co.com.salondebelleza.belleza.empleado.values;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class ContratoId extends Identity {
+    public ContratoId(String id) {
+        super(id);
+    }
+}
