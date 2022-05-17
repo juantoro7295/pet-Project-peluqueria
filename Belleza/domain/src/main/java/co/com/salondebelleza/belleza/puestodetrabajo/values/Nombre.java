24e5ff23b29dee38da2effe7d5ffc11bf38ee04a
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-17 15:14:59 -0500

    Objeto de valor - Nombre
    Se crea objeto de valor nombre

---
+++ Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/values/Nombre.java
@@ -0,0 +1,23 @@
+package co.com.salondebelleza.belleza.puestodetrabajo.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Nombre implements ValueObject<String> {
+
+    private final String value;
+
+    public Nombre(String value) {
+        this.value = Objects.requireNonNull(value);
+        if (this.value.isBlank()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacio");
+        }
+    }
+
+    @Override
+    public String value() {
+
+        return value;
+    }
+}
