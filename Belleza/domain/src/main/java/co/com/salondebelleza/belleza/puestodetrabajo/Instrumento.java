ab4116e4ca104c912db3e5d028b79408f7962bdb
Juanes07 <juanesdm3@gmail.com>
2022-05-17 16:27:24 -0500

    Entidad Instrumento
    Se creo la entidad Instrumento

---
+++ Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/Instrumento.java
@@ -0,0 +1,9 @@
+package co.com.salondebelleza.belleza.puestodetrabajo;
+
+import co.com.sofka.domain.generic.Entity;
+
+public class Instrumento extends Entity<InstrumentoId> {
+    public Instrumento(InstrumentoId id) {
+        super(id);
+    }
+}
---
+++ Belleza/domain/src/main/java/co/com/salondebelleza/belleza/puestodetrabajo/InstrumentoId.java
@@ -0,0 +1,6 @@
+package co.com.salondebelleza.belleza.puestodetrabajo;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class InstrumentoId extends Identity {
+}
